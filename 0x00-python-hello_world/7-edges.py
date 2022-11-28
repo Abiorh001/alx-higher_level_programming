31be5c24a26baa51f33e4d1f0d0a0cc341b04c60
Abiorh001 <abiolaadedayo1993@gmail.com>
2022-11-28 21:07:27 +0000

    task

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(f"First 3 letters: {word[0:3]}")
+# To printlast two words
+print(f"Last 2 letters: {word[7:9]}")
+# to print the middle words
+print(f"Middle word: {word[1:8]}")
