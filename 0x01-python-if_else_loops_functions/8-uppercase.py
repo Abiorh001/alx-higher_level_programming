30152db9feec73b1dd2a7d8693988ca30785d725
Abiorh001 <abiolaadedayo1993@gmail.com>
2022-11-29 06:09:03 -0800

    tasks two

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ''
+    for ch in str:
+        if ord(ch) >= 65:
+            result += chr(ord(ch) - 32)
+        return result
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    number = str(number)
+    return number[-1]
