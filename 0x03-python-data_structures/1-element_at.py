e19b93db504a484609b562ed939c016516aebc16
Abiorh001 <abiolaadedayo1993@gmail.com>
2022-12-02 23:18:20 -0800

    tasks four

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
+def element_at(my_list, idx):
     if idx < 0:
-        return my_list
+        return None
     elif idx > len(my_list):
-        return my_list
+        return None
     else:
-        my_list[idx] = element
-        return my_list
+        return my_list[idx]
