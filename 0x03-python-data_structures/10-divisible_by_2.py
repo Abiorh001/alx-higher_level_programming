fddcb5d97d56171a8a83b5e447a392e068b75f8b
Abiorh001 <abiolaadedayo1993@gmail.com>
2022-12-05 05:29:55 -0800

    tasks four

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    for i in my_list:
-        if i % 2 == 0:
-            print("{:d} is divisible by 2".format(i))
-        else:
-            print("{:d} is not divisible by 2".format(i))
+    newlist = [item % 2 == 0 for item in my_list]
+    return newlist
