baa5008a64fb67bc8533b302d8b9d03b5580d1f4
Abiorh001 <abiolaadedayo1993@gmail.com>
2023-01-02 07:33:10 -0800

    more classes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -26,7 +26,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-        
+
     @height.setter
     def height(self, value):
         '''setter for height'''
