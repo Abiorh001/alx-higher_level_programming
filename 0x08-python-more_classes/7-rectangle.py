70578c2ec2381be1f6b3cdad8ee07d5c0759ebc0
Abiorh001 <abiolaadedayo1993@gmail.com>
2023-01-02 08:43:21 -0800

    more  classes

---
+++ 0x08-python-more_classes/7-rectangle.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+'''creating rectangleclass'''
+
+
+class Rectangle:
+    '''RECTANGLE CLASS'''
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
+
+    @property
+    def width(self):
+        '''getter for widght'''
+        return self.__width
+
+    @property
+    def height(self):
+        '''getter for height'''
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        '''setter for width'''
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        '''setter for height'''
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        return (self.width + self.height) * 2
+
+    def __str__(self):
+        """ Return string to print rectangle with # """
+        if self.width == 0 or self.height == 0:
+            return ''
+        to_print = ''
+        for col in range(self.height):
+            for row in range(self.width):
+                to_print += str(self.print_symbol)
+            if col != self.height - 1:
+                to_print += '\n'
+        return to_print
+
+    def __repr__(self):
+        return 'Rectangle({}, {})'.format(self.width, self.height)
