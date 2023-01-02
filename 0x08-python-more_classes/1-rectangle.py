443d3cd31117326fcf8409ac3e2731ce61b2ffaa
Abiorh001 <abiolaadedayo1993@gmail.com>
2023-01-02 07:02:18 -0800

    more classes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -21,7 +21,6 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-        return self.__width
 
     @property
     def height(self):
@@ -36,4 +35,3 @@ class Rectangle:
         if self.value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-        return self.__height
