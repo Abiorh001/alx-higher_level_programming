fc03f23b2b69618ad60b27935c85d83b273b2d49
Abiorh001 <abiolaadedayo1993@gmail.com>
2023-01-02 07:57:39 -0800

    more classes

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,40 +1,42 @@
-#!/usr/bin/python3
-'''CREATING RECTANGLE CLASS WITH INSTANCES'''
-
-
 class Rectangle:
+    '''RECTANGLE CLASS'''
+
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        if type(self.__width) != int:
-            raise TypeError("width must be an integer")
-        if self.__width < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
+        '''getter for widght'''
         return self.__width
 
     @property
     def height(self):
+        '''getter for height'''
         return self.__height
+
+    @width.setter
+    def width(self, value):
+        '''setter for width'''
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
     @height.setter
     def height(self, value):
-        if type(self.__height) != int:
+        '''setter for height'''
+        if type(value) != int:
             raise TypeError("height must be an integer")
-        if self.__height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-        return self.__height
 
     def area(self):
-        return self.__width * self.__height
+        return self.width * self.height
 
     def perimeter(self):
-        return self.__width * 2 + self.__height * 2
+        if self.width == 0 or self.width == 0:
+            return 0
+        return self.width * 2 + self.height * 2
