7fa9268d2e5f21aeece4a0b70f2c22e89b4c888e
Abiorh001 <abiolaadedayo1993@gmail.com>
2023-02-15 05:25:48 -0800

    more querries

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- Script that creates an user and a database in MySQL server
+-- Query to create the user 'user_0d_1' and the database 'hbtn_0d_2' in MySQL server
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
