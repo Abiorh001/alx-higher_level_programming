c825029ca7d37940bc3029f83e0307ca5e36e545
Abiorh001 <abiolaadedayo1993@gmail.com>
2023-03-13 19:07:36 -0700

    javascript warm up

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const myVar = process.argv.length;
+if (myVar < 3) {
+  console.log('No argument');
+} else if (myVar === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
