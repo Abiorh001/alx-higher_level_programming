c84a8fe0b90f35a151f64438597f134549e98a38
Abiorh001 <abiolaadedayo1993@gmail.com>
2023-03-17 02:18:30 -0700

    MySQLdb

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
         print("error connecting")
     cur = connection.cursor()
     try:
-        cur.execute("SELECT * FROM states WHERE BINARY name = %s\
-ORDER BY states.id",(sys.argv[4],))
+        cur.execute("SELECT * FROM states WHERE BINARY name = '{}'\
+ORDER BY states.id".format(sys.argv[4],))
         rows = cur.fetchall()
         for row in rows:
             print(row)
