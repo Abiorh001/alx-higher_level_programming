b3d8c366e8c2618e97f3429194ce111fc161a42d
Abiorh001 <abiolaadedayo1993@gmail.com>
2023-03-17 02:05:37 -0700

    MySQLdb

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
-
+""" selecting with mysqldb """
 import MySQLdb
 import sys
 
-'''script that list states from the database
-   where name start from N
-'''
 
 if __name__ == "__main__":
     try:
-      db = MySQLdb.connect(host = "localhost", user = sys.argv[1],\
-passwd = sys.argv[2], port = 3306, db = sys.argv[3])
-    except MySQL.ERROR:
-      print("error connecting to database")
-    cur = db.cursor()
+        connection = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            port=3306,
+            db=sys.argv[3]
+        )
+    except MySQLdb.Error:
+        print("error connecting")
+    cur = connection.cursor()
     try:
-      cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
-      results = cur.fetchall()
-      for result in results:
-          print(result)
-    except MySQL.ERROR:
-      print("error in query the database")
+        cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+    except MySQLdb.Error:
+        print("execution failed")
     cur.close()
-    db.close()    
-    
+    connection.close()
