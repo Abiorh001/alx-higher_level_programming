3e381ccc7569a0f1350336ad6aedc97ec402afcb
Abiorh001 <abiolaadedayo1993@gmail.com>
2023-03-17 23:17:42 -0700

    mysqldb

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     cur = connection.cursor()
     cur.execute("SELECT cities.name FROM cities\
  INNER JOIN states ON cities.state_id = states.id\
-WHERE states.name = %s ORDER BY cities.id", (sys.argv[4],))
+ WHERE states.name = %s ORDER BY cities.id", (sys.argv[4],))
     rows = cur.fetchall()
     print(", ".join([row[0] for row in rows]))
     cur.close()
