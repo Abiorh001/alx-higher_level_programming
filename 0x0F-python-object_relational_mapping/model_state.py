526a0e0408829eb8999ef5a5b6f4ee44682cd999
Abiorh001 <abiolaadedayo1993@gmail.com>
2023-03-18 00:16:43 -0700

    sqllachemy model for state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,7 +2,8 @@
 """ SQLACHEMY MODEL """
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Sequence, create_engine
+from sqlalchemy.orm import sessionmaker
 
 
 Base = declarative_base()
@@ -10,5 +11,6 @@ Base = declarative_base()
 
 class State(Base):
     __tablename__ = "states"
-    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    id = Column(
+         Integer, Sequence('my_sequence'), nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
